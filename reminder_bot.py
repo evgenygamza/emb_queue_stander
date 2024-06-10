a3dfa01296d3296d061543e9ee881914214ce154
Evgenygamza <evgeny.gamza@gmail.com>
2024-06-11 00:49:05 +0400

    created messages.py

--- reminder_bot.py
+++ reminder_bot.py
@@ -41,12 +41,10 @@ async def update_queue_position(update: Update, context: ContextTypes.DEFAULT_TY
                 context.job_queue.run_once(reminder, when=3600, chat_id=chat_id, name="banned_case")
 
             pstn = await script.go_to_waiting_list_and_check_position()
-            # await context.bot.send_message(chat_id=chat_id, text=f"Текущее место в очереди: {pstn}")
             await context.bot.send_message(chat_id=chat_id, text=messages.position.replace("$value", str(pstn)))
             db_client.update(position=pstn)
 
             if pstn < 100:
-                # await context.bot.send_message(chat_id=chat_id, text="Внимание! \n Осталось меньше ста человек")
                 await context.bot.send_message(chat_id=chat_id, text=messages.last_hundred)
             result_message = await script.update_queue_position()
             await context.bot.send_message(chat_id=chat_id, text=result_message)
@@ -55,14 +53,12 @@ async def update_queue_position(update: Update, context: ContextTypes.DEFAULT_TY
 async def get_user_position(update: Update, context: ContextTypes.DEFAULT_TYPE):
     CHAT_ID = update.effective_chat.id
     with NeonConnect(dsn=DB_CONNECTION, chat_id=CHAT_ID) as db_client:
-        pos = db_client.fetch_user_info("position")[0]
-    # await context.bot.send_message(chat_id=CHAT_ID, text=f"С последнего обновления ваш номер был {pos}")
+        pos = str(db_client.fetch_user_info("position")[0])
     await context.bot.send_message(chat_id=CHAT_ID, text=messages.get_position.replace("$value", pos))
 
 
 async def new_password(update: Update, context: ContextTypes.DEFAULT_TYPE):
     CHAT_ID = update.effective_chat.id
-    # await context.bot.send_message(chat_id=CHAT_ID, text="Введите в следующем сообщении пароль:")
     await context.bot.send_message(chat_id=CHAT_ID, text=messages.enter_password)
     return GET_PASSWORD
 
@@ -72,14 +68,12 @@ async def get_new_password(update: Update, context: ContextTypes.DEFAULT_TYPE):
     new_passw = update.message.text
     with NeonConnect(dsn=DB_CONNECTION, chat_id=CHAT_ID) as db_client:
         new_passw_from_db = db_client.update(passw=new_passw)
-    # await context.bot.send_message(chat_id=CHAT_ID, text=f"Новый пароль: {new_passw_from_db}")
     await context.bot.send_message(chat_id=CHAT_ID, text=messages.new_password.replace("$value", new_passw_from_db))
     return ConversationHandler.END
 
 
 async def new_email(update: Update, context: ContextTypes.DEFAULT_TYPE):
     CHAT_ID = update.effective_chat.id
-    # await context.bot.send_message(chat_id=CHAT_ID, text="Введите в следующем сообщении адрес электронной почты:")
     await context.bot.send_message(chat_id=CHAT_ID, text=messages.enter_email)
     return GET_EMAIL
 
@@ -94,7 +88,6 @@ async def get_new_email(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # await context.bot.send_message(chat_id=update.effective_chat.id, text="Действие отменено")
     await context.bot.send_message(chat_id=update.effective_chat.id, text=messages.cancel)
     return ConversationHandler.END
 
@@ -108,7 +101,6 @@ async def delete_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 db_client.delete_user_info()
                 user_exists = db_client.check_user_exists()
             except:
-                # await context.bot.send_message(chat_id=CHAT_ID, text="Что-то пошло не так. Попробуйте еще раз: /delete")
                 await context.bot.send_message(chat_id=CHAT_ID, text=messages.delete_failed)
         if not user_exists:
             await context.bot.send_message(chat_id=CHAT_ID, text=messages.deleted)
@@ -116,8 +108,6 @@ async def delete_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def reminder(context: ContextTypes.DEFAULT_TYPE):
     job = context.job
-    # message_text = ("Пора подтвердить место в очереди. \n"
-    #                 "Пожалуйста, отправь мне команду /queue")
     await context.bot.send_message(job.chat_id, messages.reminder)
 
 
@@ -129,7 +119,6 @@ async def daily_reminder(context: ContextTypes.DEFAULT_TYPE):
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # await context.bot.send_message(chat_id=update.effective_chat.id, text=f"При чем тут {update.message.text}?")
     await context.bot.send_message(chat_id=update.effective_chat.id,
                                    text=messages.echo.replace("$value", update.message.text))
 
